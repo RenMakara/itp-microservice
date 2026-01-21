2112f52e6679190fcab1df599fa571c53f1a4c47
Ren Makara <makara00814@gmail.com>
2026-01-22 00:13:28 +0700

    Initialize admin-bff, front-bff, and e-banking-front projects with essential configurations and components
    Added `.gitignore` and `.gitattributes` files for all projects to standardize file handling and ignore unnecessary files. Configured `build.gradle` for both BFFs to include dependencies for Spring Cloud Gateway, OAuth2, and Eureka Client. Implemented initial application YAML files to define service-specific configurations like port, profile, and security settings. Created foundational React components (`Accordion`, `Button`, `Card`, `Avatar`, and `Carousel`) along with a `FooterSection` for e-banking-front.

---
+++ admin-bff/src/main/java/co/istad/makara/ebanking/AdminBffApplication.java
@@ -0,0 +1,13 @@
+package co.istad.makara.ebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBffApplication.class, args);
+	}
+
+}
---
+++ front-bff/src/test/java/co/istad/makara/ebanking/FrontBffApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.makara.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
