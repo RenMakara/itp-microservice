1388f802f31805f926c8a84a47f5a009ce8ef9c3
RenMakara <makara00814@gmail.com>
2026-02-09 02:05:45 +0700

    Add Avro schema for XmlRecord and Key, stream configuration for envelope processing in Debezium events

--- pipeline-service/src/main/java/co/istad/makara/pipeline/stream/DbzStreamConfig.java
+++ pipeline-service/src/main/java/co/istad/makara/pipeline/stream/DbzStreamConfig.java
@@ -1,5 +1,6 @@
 package co.istad.makara.pipeline.stream;
 
+import ITP.CORE_BANKING.RECORD_XML.Envelope;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +18,50 @@ public class DbzStreamConfig {
     ///  instead of GenericRecord directly, because when delete event happens,
     ///  the payload will be null, but the header will still exist, so we can
     /// check the header to know that it's a delete event and handle it accordingly.
+
+    @Bean
+    public Consumer<Message<Envelope>> captureEnvelope(ObjectMapper objectMapper){
+        return record -> {
+            try {
+                // Check for tombstone delete event (null payload)
+                if (record.getPayload() == null) {
+                    System.out.println("Tombstone delete event detected");
+                    Object key = record.getHeaders().get("kafka_receivedMessageKey");
+                    System.out.println("Deleted Record Key: " + key);
+                    return;
+                }
+
+                DebeziumEnvelope<Envelope> captureEnvelope =
+                        objectMapper.readValue(record.getPayload().toString(),
+                                new TypeReference<>() {
+                                });
+
+                switch (captureEnvelope.getOp()) {
+                    case "c", "r" -> {
+                        System.out.println("Prepare to insert new envelope...");
+                        Envelope after = captureEnvelope.getAfter();
+                        System.out.println("After Envelope: " + after);
+
+                    }
+                    case "u" -> {
+                        Envelope after = captureEnvelope.getAfter();
+                        System.out.println("Prepare to update envelope...");
+                        System.out.println("Before: " + captureEnvelope.getBefore());
+
+                    }
+                    case "d" -> {
+                        System.out.println("Prepare to delete envelope...");
+                        System.out.println("Deleted Envelope: " + captureEnvelope.getBefore());
+
+                    }
+                    default -> throw new IllegalStateException("Invalid operation: " + captureEnvelope.getOp());
+                }
+            } catch (JsonProcessingException e) {
+                System.out.println("Error deserialize envelope!!");
+            }
+        };
+    }
+
 
     @Bean
     public Function<Message<Object>, Record> consumeDbzEvent(ObjectMapper objectMapper){
